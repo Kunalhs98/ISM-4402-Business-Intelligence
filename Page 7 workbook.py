1b32978e6c5dae23214deee76239177d61d87d41
Kunalhs98 <70179478+Kunalhs98@users.noreply.github.com>
2020-09-06 16:49:39 -0700

    Add files via upload

---
+++ Page 7 workbook.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+cd C:\Users\Kunal Samtani\Desktop\ISM 4402
+
+
+# In[3]:
+
+
+df = pd.read_csv("all_040_in_01.P1.csv")
+
+
+# In[4]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
---
+++ Page 9 workbook.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+cd C:\Users\Kunal Samtani\Desktop\ISM 4402
+
+
+# In[3]:
+
+
+df = pd.read_excel("AGE01.xls")
+
+
+# In[4]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
